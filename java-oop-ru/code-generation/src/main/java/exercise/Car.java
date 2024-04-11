ebd805a58deeaedd312f7b9fb00c966523af1682
@hexlet/cli <support@hexlet.io>
2024-04-11 21:40:46 +0300

    submit java-oop-ru/code-generation

---
+++ java-oop-ru/code-generation/src/main/java/exercise/Car.java
@@ -0,0 +1,40 @@
+package exercise;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.io.IOException;
+
+// BEGIN
+@Data
+@AllArgsConstructor
+// END
+class Car {
+    int id;
+    String brand;
+    String model;
+    String color;
+    User owner;
+
+    // BEGIN
+    public String serialize() {
+        ObjectMapper objectMapper = new ObjectMapper();
+        try {
+            return objectMapper.writeValueAsString(this);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static Car unserialize(String json) {
+        ObjectMapper objectMapper = new ObjectMapper();
+        try {
+            return objectMapper.readValue(json, Car.class);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    // END
+}
